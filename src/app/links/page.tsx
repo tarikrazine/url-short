0c419bbfffe42f19500afd7bd883312800351389
tarik razine <tarikrazine.om@gmail.com>
2023-08-14 23:50:30 +0100

    feat: revalidate

--- src/app/links/page.tsx
+++ src/app/links/page.tsx
@@ -2,6 +2,8 @@ import { db } from "@/lib/db"
 import { linksTable } from "@/schema/links"
 import { AddLink } from "@/components/AddLink"
 
+export const revalidate = 10
+
 export async function getLinks(limit = 10, offset = 0) {
     const data = await db.select().from(linksTable).limit(limit).offset(offset)
 
